c1c52f8db26f5c31ca6431bc0c364e20f0264f61
Lacquered <marshalnsk22854@gmail.com>
2022-07-06 17:08:45 +0700

    1) Rename main class 2) Add javax-validation on pom.xml

--- src/main/java/ru/cft/croudfounding/CrowdfundingPlatformApplication.java
+++ src/main/java/ru/cft/croudfounding/CrowdfundingPlatformApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CroudfoundingApplication {
+public class CrowdfundingPlatformApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CroudfoundingApplication.class, args);
+		SpringApplication.run(CrowdfundingPlatformApplication.class, args);
 	}
 
 }
