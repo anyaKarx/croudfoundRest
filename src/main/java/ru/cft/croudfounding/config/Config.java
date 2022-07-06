799fed260986c93abe7dff243147ec9540921417
somnus <steeltouch27@gmail.com>
2022-07-06 17:47:35 +0700

    1) Renamed config class 2) Added PasswordEncoder bean 3) Added Spring Security dependency

--- src/main/java/ru/cft/croudfounding/config/Config.java
+++ src/main/java/ru/cft/croudfounding/config/Config.java
@@ -2,13 +2,15 @@ package ru.cft.croudfounding.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SpringFoxConfig {
+public class Config {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -16,6 +18,11 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
 
